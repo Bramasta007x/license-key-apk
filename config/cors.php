d7e5d85f39c223d49af1ed9cfa47d4b6a2b82944
Bramasta007x <bramastaalbatio007x@gmail.com>
2026-03-18 13:11:21 +0700

    add licesene key api

--- config/cors.php
+++ config/cors.php
@@ -12,7 +12,7 @@ return [
 
     'allowed_origins' => [
         'http://127.0.0.1:3000',
-        'http://localhost:3000',
+        'http://localhost:5173',
         'https://fe.eticket-jayapura.com',
          env('FRONTEND_URL', 'https://jayapuramusicfest.com'),
     ],
